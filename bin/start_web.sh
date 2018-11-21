814c491cf859079af1b2da067111dfe0baa566c9
Udall Hu <xmudall@gmail.com>
2018-11-21 14:18:06 +0800

    add web script

---
+++ bin/start_web.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BIN_HOME=$(cd "$(dirname "$0")"; pwd)
+cd $BIN_HOME/../src
+export FLASK_APP=web.app
+sub=`date +%Y%m%d`
+pidfile=/tmp/flask.pid
+logfile=/tmp/log/flask_$sub.log
+if [ ! -d /tmp/log ]; then
+    mkdir /tmp/log
+fi
+
+p3
+
+nohup python -m flask run 2>&1 > $logfile & pid=$!
+
+echo "process flask pid "$pid" write to "$pidfile
+echo $pid > $pidfile
