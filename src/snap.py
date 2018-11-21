88dbe188048720ac375204837c92e4aad83816b5
Udall Hu <xmudall@gmail.com>
2018-11-21 13:41:46 +0800

    update snap

--- src/snap.py
+++ src/snap.py
@@ -1,9 +1,13 @@
 from time import sleep
 from picamera import PiCamera
+import os
+
+image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'web/static/ss.jpg')
+print('config path: {}'.format(image_path))
 
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+for filename in camera.capture_continuous(image_path):
     print('Captured %s' % filename)
-    sleep(300) # wait 5 minutes
+    sleep(5) # wait 5 minutes
